87e6edb350ef8cec0b651024177eedff3e93703d
Dan G5DSG <dan@g5dsg.net>
2024-08-04 22:20:02 +0100

    initial work for multi-device support
    All commands should now accept --serial XXX to work with the specified
    device.
    
    Note that you must first enable serial number enumeration with
    only 1 MPC2221a device connected for this to work

--- 2221aTool.py
+++ 2221aTool.py
@@ -1,5 +1,4 @@
 import click
-import code
 import re
 import sys
 import usb.core
@@ -7,36 +6,48 @@ import lib.commands as cmds
 from lib.utils import get_bit
 
 
-# to-do enable finding of specific device via /dev/ttyACMX command instead of USB ids
-config = {"idVendor": 0x04D8, "idProduct": 0x00DD, "hidDevice": 2}
-
-# to-do pass these via optional command line
-# until then, in case of emergency...
-# config = {"idVendor": 0x0000, "idProduct": 0x0000, "hidDevice": 2}
-
-dev = usb.core.find(idVendor=config["idVendor"], idProduct=config["idProduct"])
-
-try:
-    dev.detach_kernel_driver(config["hidDevice"])
-except usb.core.USBError:
-    pass  # its already detached...
-
-# pick the first matching device we find
-# to-do: detect specific nino by its /dev/ node
-d = dev[0]
-dev.reset()
-hid = d[(2, 0)]
-
-
 @click.group()
-def cli():
-    pass
+@click.option("--serial", "-s", default=None, help="Device USB serial number")
+@click.option("--vendor", default="0x04D8")
+@click.option("--product", default="0x00DD")
+@click.pass_context
+def cli(ctx, serial, vendor, product):
+    ctx.obj = dict()
+    ctx.obj["serial"] = serial
+    ctx.obj["vendor"] = int(vendor[-4:], 16)
+    ctx.obj["product"] = int(product[-4:], 16)
+    ctx.obj["hid_dev"] = 2
+    ctx.obj["dev"], ctx.obj["hid"] = init_usb(
+        ctx.obj["serial"], ctx.obj["vendor"], ctx.obj["product"], ctx.obj["hid_dev"]
+    )
+
+
+def init_usb(serial, vendor, product, hid_dev):
+    if serial:
+        dev = usb.core.find(idVendor=vendor, idProduct=product, serial_number=serial)
+    else:
+        dev = usb.core.find(idVendor=vendor, idProduct=product)
+
+    try:
+        dev.detach_kernel_driver(hid_dev)
+    except usb.core.USBError:
+        pass  # its already detached...
+    except AttributeError:
+        sys.exit("Device not found?")
+
+    d = dev[0]
+    dev.reset()
+    hid = d[(2, 0)]
+    return dev, hid
 
 
 @cli.command()
 @click.argument("filename", type=click.Path(exists=False))
-def dump_flash(filename):
+@click.pass_context
+def dump_flash(ctx, filename):
     """Dump the current flash config to a file for 'emergencies'."""
+    hid = ctx.obj["hid"]
+    dev = ctx.obj["dev"]
     hid.endpoints()[1].write(cmds.dumpFlash)
     response = hid.endpoints()[0].read(64)
     # to-do - verify good response...
@@ -54,23 +65,29 @@ def dump_flash(filename):
 
 @cli.command()
 @click.argument("filename", type=click.Path(exists=True))
-def restore_flash(filename):
+@click.pass_context
+def restore_flash(ctx, filename):
     """Upload save flash config file to device."""
+    hid = ctx.obj["hid"]
+    dev = ctx.obj["dev"]
     with open(filename, "rb") as f:
         config = f.read()
         f.close()
     print(f"loaded flash config {config}")
-    newConfig = config + bytes([0] * 8)
-    hid.endpoints()[1].write(cmds.writeFlash + newConfig)
-    ## to-do - check response
+    new_config = config + bytes([0] * 8)
+    hid.endpoints()[1].write(cmds.writeFlash + new_config)
+    # to-do - check response
     click.secho("done")
     dev.reset()
 
 
 @cli.command()
 @click.argument("state", type=click.Choice(["0", "1"]))
-def enum_serial(state):
+@click.pass_context
+def enum_serial(ctx, state):
     """Toggle serial number enumeration."""
+    hid = ctx.obj["hid"]
+    dev = ctx.obj["dev"]
     state = int(state)
     hid.endpoints()[1].write(cmds.dumpFlash)
     response = hid.endpoints()[0].read(64)
@@ -84,64 +101,78 @@ def enum_serial(state):
         click.secho(f"current state already {current_state}")
     else:
         response[4] = response[4] ^ 0b10000000
-        newConfig = bytes(response[4:14]) + bytes([0] * 8)
-        hid.endpoints()[1].write(cmds.writeFlash + newConfig)
+        new_config = bytes(response[4:14]) + bytes([0] * 8)
+        hid.endpoints()[1].write(cmds.writeFlash + new_config)
         # to-do - check response
         click.secho("done")
     dev.reset()
 
 
 @cli.command()
-@click.argument("serial")
-def set_serial(serial):
+@click.argument("new_serial")
+@click.pass_context
+def set_serial(ctx, new_serial):
     """Sets the device serial number.
     Any unicode string of <=30 chars
     """
-    if len(serial) > 30:
+    hid = ctx.obj["hid"]
+    if len(new_serial) > 30:
         raise ValueError
 
-    serialBytes = serial.encode("utf-16-le")
-    serialLen = len(serialBytes) + 2
-    hid.endpoints()[1].write(cmds.setSerial + bytes([serialLen, 0x03]) + serialBytes)
+    serial_bytes = new_serial.encode("utf-16-le")
+    serial_len = len(serial_bytes) + 2
+    hid.endpoints()[1].write(cmds.setSerial + bytes([serial_len, 0x03]) + serial_bytes)
     click.secho("done, please disconnect/reconnect the device to see changes")
+
 
 @cli.command()
 @click.argument("desc")
-def set_description(desc):
+@click.pass_context
+def set_description(ctx, desc):
     """Sets the device description.
     Any unicode string of <=30 chars
     """
+    hid = ctx.obj["hid"]
     if len(desc) > 30:
         raise ValueError
 
-    descBytes = desc.encode("utf-16-le")
-    descLen = len(descBytes) + 2
-    hid.endpoints()[1].write(cmds.setDescription + bytes([descLen, 0x03]) + descBytes)
+    desc_bytes = desc.encode("utf-16-le")
+    desc_len = len(desc_bytes) + 2
+    hid.endpoints()[1].write(cmds.setDescription + bytes([desc_len, 0x03]) + desc_bytes)
     click.secho("done, please disconnect/reconnect the device to see changes")
 
 
 @cli.command()
-def setup_gpio():
-    """Write flash config to set all GPIO pins to output / low at startup"""
+#@click.argument("state", default="0000")
+@click.pass_context
+def setup_gpio(ctx):
+    """Set all GPIO pins"""
+    hid = ctx.obj["hid"]
     hid.endpoints()[1].write(cmds.setupGPIO)
     response = hid.endpoints()[0].read(64)
     click.secho("ok")
+
+
 @cli.command()
 @click.argument("state")
-def set_gpio(state):
+@click.pass_context
+def set_gpio(ctx, state):
     """Set GPIO state"""
+    hid = ctx.obj["hid"]
+
     if not re.match(r"^[01][01][01][01]$", state):
         raise ValueError
-    cmd = cmds.setGPIO
+    cmd = cmds.setGPIO_temp
     for b in state:
         if b == "1":
-            cmd=cmd+bytes([0x01,0x01,0x00,0x00])
+            cmd = cmd + bytes([0x01, 0x01, 0x00, 0x00])
         else:
-            cmd=cmd+bytes([0x01,0x00,0x00,0x00])
+            cmd = cmd + bytes([0x01, 0x00, 0x00, 0x00])
 
     hid.endpoints()[1].write(cmd)
     response = hid.endpoints()[0].read(64)
     print(response)
 
+
 if __name__ == "__main__":
-    cli()
+    cli(obj={})
--- lib/commands.py
+++ lib/commands.py
@@ -2,10 +2,13 @@ dumpFlash = bytes([0xB0, 0x00])
 writeFlash = bytes([0xB1, 0x00])
 setSerial = bytes([0xB1, 0x04])
 
-#Configure GPIO power status - all pins as output / low at startup
+# deprecated - Configure GPIO power status - all pins as output / low at startup
 setupGPIO = bytes([0xb1, 0x01, 0x00, 0x00, 0x00, 0x00])
 
-setGPIO  = bytes([0x50, 0x00])
-##to-do - implement these:
+# new commands to-do...
+setGPIO_perm = bytes([0xb1, 0x01])
+setGPIO_temp = bytes([0x50, 0x00])
+
+# to-do - implement these:
 getFactorySerial = bytes([0xB0, 0x05])
 setDescription = bytes([0xB1, 0x03])
